f4ae88fe3682e57d28738c89934b04d652cc648a
Yoav Grimland <miestrode@proton.me>
2023-10-13 09:47:09 +0300

    Remove NeoGit

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -37,14 +37,4 @@ return {
             },
         },
     },
-    {
-        "NeogitOrg/neogit",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim",
-            "sindrets/diffview.nvim",
-            "ibhagwan/fzf-lua",
-        },
-        config = true,
-    },
 }
