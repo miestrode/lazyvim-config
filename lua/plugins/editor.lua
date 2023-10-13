e6b8b252f81cd3d602628d05b5cf6fd4c45fce7f
Yoav Grimland <miestrode@proton.me>
2023-10-13 10:03:47 +0300

    Switched to NeoGit and updated keymaps accordingly

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -37,4 +37,14 @@ return {
             },
         },
     },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "sindrets/diffview.nvim",
+            "ibhagwan/fzf-lua",
+        },
+        config = true,
+    },
 }
