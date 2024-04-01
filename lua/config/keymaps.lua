2d7c56b00b76abacc4544aa002b40320c872970e
Yoav Grimland <miestrode@proton.me>
2024-04-01 16:47:51 +0300

    Add support for inlay hints, sync CWD with that in the dirtree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>gd", "DiffviewOpen")
+vim.keymap.set("n", "<leader>ci", function()
+    vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
+end, { desc = "Toggle inlay hints" })
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -23,6 +23,7 @@ return {
         "nvim-neo-tree/neo-tree.nvim",
         opts = {
             filesystem = {
+                bind_to_cwd = true,
                 filtered_items = {
                     visible = true,
                     hide_dotfiles = false,
